a87a2d40243bb805cf076d36d7ee4f04489af632
kirstinlyon <kirstin@mltwelve.com>
2023-12-02 15:55:24 +0100

    Utilities script updated to include create_calc helper functions

--- R/utilities.R
+++ R/utilities.R
@@ -5,7 +5,10 @@
 #'
 #' @return returns wanted xpath fields as a tibble
 #' @export
-#'
+#' @examples
+#'  \dontrun{
+#'    convert_cols_xml_to_tbl(data; twb_xpath)
+#' }#'
 convert_cols_xml_to_tbl <- function(data,twb_xpath){
 
     tryCatch(
@@ -19,4 +22,57 @@ convert_cols_xml_to_tbl <- function(data,twb_xpath){
         }
     )
 
+}
+
+
+#' Checks the "caption" created to ensure no duplicates
+#'
+#' @param twb_file a twb file
+#' @param a_name a name for a calculation
+#'
+#' @return returns TRUE/FALSE depending on if the name exists in the dataset
+#' @export
+#'
+#' @examples
+#'  \dontrun{
+#'    check_name(twb_file, a_name)
+#' }
+check_name <- function(twb_file, a_name){
+
+    all_calc_names <- twb_file %>%
+        TWB2R::all_other_created() %>%
+        dplyr::select(name)
+
+    all_param_names <- twb_file %>%
+        TWB2R::all_parameters() %>%
+        dplyr::select(name)
+
+    all_raw_names <- twb_file %>%
+        TWB2R::all_raw_fields() %>%
+        dplyr::select(name)
+
+    all_names <- dplyr::bind_rows(all_calc_names,
+                                  all_param_names,
+                                  all_raw_names) %>%
+        dplyr::pull(name)
+
+    return (a_name %in% all_names)
+}
+
+#' create unique ID for the calculation.
+#'
+#' @param twb_file twb_file
+#'
+#' @return a unique code for a calculation
+#' @export
+#'
+#' @examples
+#'  \dontrun{
+#'    create_calc_id(twb_file)
+#' }
+create_calc_id <- function(twb_file){
+    all_ids <- twb_file %>%
+        TWB2R::all_other_created() %>%
+        dplyr::select(unique_id) %>%
+        dplyr::pull(unique_id)
 }
