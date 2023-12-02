a315d26cb8e1eecc70980f45da68323b30d4b8ee
kirstinlyon <kirstin@mltwelve.com>
2023-12-02 15:42:42 +0100

    create_calc function added.

---
+++ R/add_calc.R
@@ -0,0 +1,41 @@
+#' Add a calculation to a Tableau workbook
+#'
+#' @param twb_file A tableau file
+#' @param file_location location of where the twb file is stored
+#' @param caption user-friendly name
+#' @param datatype string, number, date, ...
+#' @param role measure or dimension
+#' @param type nominal, ordinal or ...
+#' @param class calculations are a type tableau
+#' @param formula formula for the calculation
+#'
+#' @return an updated twb_file with an added calculation
+#' @export
+#'
+#' @examples
+#'  \dontrun{
+#'    create_calc(twb_file, file_Location, caption, datatype,
+#'    role, type, class, formula)
+#' }
+create_calc <- function(twb_file, file_location,caption, datatype,
+                        role, type, class = "Tableau", formula ){
+
+
+    datasource_nodes <- xml2::xml_find_all(file, "//datasources/datasource")
+
+    new_tag <- xml2::xml_add_child(datasource_nodes[[1]], "column")
+    child <- xml2::xml_add_child(new_tag, "calculation")
+    xml2::xml_set_attr(new_tag, "caption", caption)
+    xml2::xml_set_attr(new_tag, "datatype", datatype)
+    xml2::xml_set_attr(new_tag, "name", "[Calculation_1097752430143991829]")
+    xml2::xml_set_attr(new_tag, "role", role)
+    xml2::xml_set_attr(new_tag, "type", type)
+
+    xml2::xml_set_attr(child, "class", class)
+    xml2::xml_set_attr(child, "formula", formula)
+
+    xml2::write_xml(twb_file, file_location)
+
+    return(twb_file)
+
+}
